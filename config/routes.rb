1b896e202bbef39af2448c66a2bf606fbd1f4d60
takumaaizu <takuma.judo@gmail.com>
2017-04-18 08:18:18 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
+  root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
 end
